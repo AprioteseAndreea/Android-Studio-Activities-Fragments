30886b9fd531e52a72810bb4593146724456d55f
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-04-10 20:57:34 +0300

    Added the interface that allows to communicate between fragments and activities

---
+++ Tema1Android/app/src/main/java/com/example/tema1android/interfaces/ActivityFragmentCommunication.java
@@ -0,0 +1,11 @@
+package com.example.tema1android.interfaces;
+
+public interface ActivityFragmentCommunication {
+
+     void openSecondActivity();
+     void replaceF2A2();
+
+     void replaceF3A2();
+
+     void removeF1A2();
+}
